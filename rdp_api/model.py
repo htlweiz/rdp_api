ef7b89801bb138e7100ddfc71034d8fe37dd70f3
Robert Ulmer <robert@git.frontexperts.com>
2023-08-24 13:07:06 +0200

    initial package setup

---
+++ rdp_api/model.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Float, create_engine
+from sqlalchemy.ext import declarative
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative.declarative_base()
+
