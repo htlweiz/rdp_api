cabd534c88a6b40c395f36d9e24ce5091768c78b
Robert Ulmer <robert@git.frontexperts.com>
2023-08-30 23:21:57 +0200

    testing

--- rdp/api/main.py
+++ rdp/api/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World!"}
 
 @app.get("/type/")
 def read_types() -> List[ApiTypes.ValueType]:
