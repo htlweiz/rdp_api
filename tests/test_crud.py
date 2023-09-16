709b46d6cfadc4dd3d3396c86424de915b4b7e23
mderler <derler190063@sr.htlweiz.at>
2023-09-16 21:57:26 +0000

    Add update_value_type tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,17 +1,15 @@
 import pytest
 from sqlalchemy import create_engine, select
+from sqlalchemy.exc import NoResultFound, InterfaceError
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import NoResultFound
 from rdp.crud.model import Base, ValueType
 from rdp.crud.crud import Crud
 from typing import Any
 
 
 @pytest.fixture(scope="function")
-def crud_session():
+def crud_with_session():
     engine = create_engine("sqlite:///:memory:")
-
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
 
@@ -20,9 +18,16 @@ def crud_session():
     Base.metadata.drop_all(engine)
 
 
-def test_add_value_type_missing_parameters(crud_session: (Crud, Any)):
-    crud, _ = crud_session
+@pytest.fixture(scope="function")
+def crud():
+    engine = create_engine("sqlite:///:memory:")
 
+    yield Crud(engine)
+
+    Base.metadata.drop_all(engine)
+
+
+def test_add_value_type_missing_parameters(crud: Crud):
     value_types = ["type0", "type1", "temperature", "HUMIDITY_01!"]
     for value_type in value_types:
         with pytest.raises(TypeError):
@@ -34,8 +39,8 @@ def test_add_value_type_missing_parameters(crud_session: (Crud, Any)):
             crud.add_or_update_value_type(value_type_unit=unit)
 
 
-def test_add_value_type(crud_session: (Crud, Any)):
-    crud, Session = crud_session
+def test_add_value_type(crud_with_session: (Crud, Any)):
+    crud, Session = crud_with_session
 
     value_types = [
         {"name": "type0", "unit": "°C"},
@@ -67,8 +72,108 @@ def test_add_value_type(crud_session: (Crud, Any)):
     assert all(db_value_type in value_types for db_value_type in db_value_types)
 
 
-def test_get_value_types(crud_session: (Crud, Any)):
-    crud, Session = crud_session
+def test_update_value_type_invalid_ids(crud: Crud):
+    invalid_ids = ["foo", "12", "1", "!bar"]
+
+    for id in invalid_ids:
+        with pytest.raises(TypeError):
+            crud.add_or_update_value_type(id)
+
+    invalid_ids = [[], [1, 2, 3], ["foo"], {}, {"foo": "bar"}]
+
+    for id in invalid_ids:
+        with pytest.raises(InterfaceError):
+            crud.add_or_update_value_type(id)
+
+
+def test_add_value_type_default_values(crud_with_session):
+    crud, Session = crud_with_session
+
+    ids = [1, 2, 3, 110]
+    for id in ids:
+        crud.add_or_update_value_type(id)
+
+    result = None
+    with Session() as session:
+        query = select(ValueType)
+        result = session.scalars(query).all()
+
+    for value_type in result:
+        assert value_type.type_name == "TYPE_%d" % value_type.id
+        assert value_type.type_unit == "UNIT_%d" % value_type.id
+
+
+def test_update_value_type(crud_with_session):
+    crud, Session = crud_with_session
+
+    value_types = {
+        1: (None, None),
+        2: (None, None),
+        10: (None, "K"),
+        11: ("foo", "bar"),
+        5: ("foo", None),
+    }
+
+    for key, value_type in value_types.items():
+        crud.add_or_update_value_type(key, value_type[0], value_type[1])
+
+    result = None
+    with Session() as session:
+        query = select(ValueType)
+        result = session.scalars(query).all()
+
+    for db_value_type in result:
+        value_type = value_types[db_value_type.id]
+        if value_type[0] is None:
+            assert db_value_type.type_name == "TYPE_%d" % db_value_type.id
+        else:
+            assert db_value_type.type_name == value_type[0]
+        if value_type[1] is None:
+            assert db_value_type.type_unit == "UNIT_%d" % db_value_type.id
+        else:
+            assert db_value_type.type_unit == value_type[1]
+
+    updated_value_types = {
+        1: (None, None),
+        2: ("foo", "bar"),
+        10: ("Temp", "K"),
+        11: ("foo", None),
+        5: ("foo", "Bazz"),
+    }
+
+    for key, value_type in updated_value_types.items():
+        crud.add_or_update_value_type(key, value_type[0], value_type[1])
+
+    with Session() as session:
+        query = select(ValueType)
+        result = session.scalars(query).all()
+
+    for db_value_type in result:
+        value_type = value_types[db_value_type.id]
+        updated_value_type = updated_value_types[db_value_type.id]
+        if updated_value_type[0] is None:
+            name = ""
+            if value_type[0] is None:
+                name = "TYPE_%d" % db_value_type.id
+            else:
+                name = value_type[0]
+
+            assert db_value_type.type_name == name
+        else:
+            assert db_value_type.type_name == updated_value_type[0]
+        if updated_value_type[1] is None:
+            unit = ""
+            if value_type[1] is None:
+                unit = "UNIT_%d" % db_value_type.id
+            else:
+                unit = value_type[1]
+            assert db_value_type.type_unit == unit
+        else:
+            assert db_value_type.type_unit == updated_value_type[1]
+
+
+def test_get_value_types(crud_with_session: (Crud, Any)):
+    crud, Session = crud_with_session
 
     value_types = [
         {"name": "type0", "unit": "°C"},
@@ -99,11 +204,11 @@ def test_get_value_types(crud_session: (Crud, Any)):
     assert all(db_value_type in value_types for db_value_type in db_value_types)
 
 
-def test_get_value_type_no_id(crud_session: (Crud, Any)):
-    crud, Session = crud_session
+def test_get_value_type_no_id(crud_with_session: (Crud, Any)):
+    crud, Session = crud_with_session
 
-    wrong_ids = [1, 12, 22, 999999999, -1, "asd", "Test Test"]
-    for id in wrong_ids:
+    invalid_ids = [1, 12, 22, 999999999, -1, "foo", "Test Test"]
+    for id in invalid_ids:
         with pytest.raises(NoResultFound):
             crud.get_value_type(id)
 
@@ -112,8 +217,8 @@ def test_get_value_type_no_id(crud_session: (Crud, Any)):
         pass
 
 
-def test_get_value_type(crud_session: (Crud, Any)):
-    crud, Session = crud_session
+def test_get_value_type(crud_with_session: (Crud, Any)):
+    crud, Session = crud_with_session
 
     value_types = [
         {"name": "type0", "unit": "°C"},
