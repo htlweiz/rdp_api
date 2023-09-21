7791d2a72c87be13bc33873bad569e025334da82
mderler <derler190063@sr.htlweiz.at>
2023-09-21 09:15:22 +0000

    Add tests for get_values

--- tests/test_get_values.py
+++ tests/test_get_values.py
@@ -3,10 +3,12 @@ from typing import Tuple, Type
 from sqlalchemy.orm import Session
 
 from rdp.crud.crud import Crud
-from rdp.crud.model import Value
+from rdp.crud.model import Value, ValueType
 
 
 CrudSession = Type[Tuple[Crud, Type[Session]]]
+
+VALUE_TYPES = [("K", "unit1"), ("m", "unit2"), ("s", "unit3")]
 
 VALUES = [
     (1, 1, 12.2),
@@ -27,6 +29,14 @@ def test_get_values_all(crud_with_session: CrudSession):
     crud, Session = crud_with_session
 
     with Session() as session:
+        db_value_types = map(
+            lambda value_type: ValueType(
+                type_unit=value_type[0], type_name=value_type[1]
+            ),
+            VALUE_TYPES,
+        )
+        session.add_all(db_value_types)
+        session.commit()
         db_values = map(
             lambda value: Value(time=value[0], value_type_id=value[1], value=value[2]),
             VALUES,
@@ -49,6 +59,14 @@ def test_get_values_by_value_type(crud_with_session: CrudSession):
     crud, Session = crud_with_session
 
     with Session() as session:
+        db_value_types = map(
+            lambda value_type: ValueType(
+                type_unit=value_type[0], type_name=value_type[1]
+            ),
+            VALUE_TYPES,
+        )
+        session.add_all(db_value_types)
+        session.commit()
         db_values = map(
             lambda value: Value(time=value[0], value_type_id=value[1], value=value[2]),
             VALUES,
@@ -57,9 +75,136 @@ def test_get_values_by_value_type(crud_with_session: CrudSession):
         session.commit()
 
     count = 0
-    for i in range(3):
-        db_values = crud.get_values(i)
+    for i in range(1, 4):
+        db_values = list(
+            map(
+                lambda db_value: (
+                    db_value.time,
+                    db_value.value_type_id,
+                    db_value.value,
+                ),
+                crud.get_values(i),
+            )
+        )
         count += len(db_values)
         assert all(db_value in VALUES for db_value in db_values)
 
     assert count == len(VALUES)
+
+
+def test_get_values_by_start(crud_with_session: CrudSession):
+    crud, Session = crud_with_session
+
+    with Session() as session:
+        db_value_types = map(
+            lambda value_type: ValueType(
+                type_unit=value_type[0], type_name=value_type[1]
+            ),
+            VALUE_TYPES,
+        )
+        session.add_all(db_value_types)
+        session.commit()
+        db_values = map(
+            lambda value: Value(time=value[0], value_type_id=value[1], value=value[2]),
+            VALUES,
+        )
+        session.add_all(db_values)
+        session.commit()
+
+    times = [1, 2, 5, 100]
+    for time in times:
+        filtered_values = [value for value in VALUES if value[0] >= time]
+        db_values = list(
+            map(
+                lambda db_value: (
+                    db_value.time,
+                    db_value.value_type_id,
+                    db_value.value,
+                ),
+                crud.get_values(start=time),
+            )
+        )
+
+        assert all(db_value in filtered_values for db_value in db_values)
+        assert all(value in db_values for value in filtered_values)
+
+
+def test_get_values_by_end(crud_with_session: CrudSession):
+    crud, Session = crud_with_session
+
+    with Session() as session:
+        db_value_types = map(
+            lambda value_type: ValueType(
+                type_unit=value_type[0], type_name=value_type[1]
+            ),
+            VALUE_TYPES,
+        )
+        session.add_all(db_value_types)
+        session.commit()
+        db_values = map(
+            lambda value: Value(time=value[0], value_type_id=value[1], value=value[2]),
+            VALUES,
+        )
+        session.add_all(db_values)
+        session.commit()
+
+    times = [1, 2, 5, 99, 100, 101, 9999, 999999]
+    for time in times:
+        filtered_values = [value for value in VALUES if value[0] <= time]
+        db_values = list(
+            map(
+                lambda db_value: (
+                    db_value.time,
+                    db_value.value_type_id,
+                    db_value.value,
+                ),
+                crud.get_values(end=time),
+            )
+        )
+
+        assert all(db_value in filtered_values for db_value in db_values)
+        assert all(value in db_values for value in filtered_values)
+
+
+def test_get_values_all_filters(crud_with_session: CrudSession):
+    crud, Session = crud_with_session
+
+    with Session() as session:
+        db_value_types = map(
+            lambda value_type: ValueType(
+                type_unit=value_type[0], type_name=value_type[1]
+            ),
+            VALUE_TYPES,
+        )
+        session.add_all(db_value_types)
+        session.commit()
+        db_values = map(
+            lambda value: Value(time=value[0], value_type_id=value[1], value=value[2]),
+            VALUES,
+        )
+        session.add_all(db_values)
+        session.commit()
+
+    type_ids = [1, 2, 3]
+    times = [1, 2, 5, 99, 100, 101, 9999, 1000000]
+    for start in times:
+        for end in times:
+            for type_id in type_ids:
+                filtered_values = [
+                    value
+                    for value in VALUES
+                    if value[0] >= start and value[0] <= end and value[1] == type_id
+                ]
+                db_values = list(
+                    map(
+                        lambda db_value: (
+                            db_value.time,
+                            db_value.value_type_id,
+                            db_value.value,
+                        ),
+                        crud.get_values(start=start, end=end, value_type_id=type_id),
+                    )
+                )
+
+                assert all(db_value in filtered_values for db_value in db_values)
+                assert all(value in db_values for value in filtered_values)
