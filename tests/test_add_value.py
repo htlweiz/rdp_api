65b190b8413fde5a4cea5b524c4e82bcb8e83df5
mderler <derler190063@sr.htlweiz.at>
2023-09-26 12:31:34 +0000

    WIP changing tests to new data model

--- tests/test_add_value.py
+++ tests/test_add_value.py
@@ -16,19 +16,21 @@ def test_add_value(crud_with_session: CrudSession):
     crud, Session = crud_with_session
 
     values = [
-        (12123, 1, -123),
-        (12, 1, -1.2),
-        (14, 1, 100),
-        (1381, 1, 9999999.99),
-        (98315, 1, -999999123.123),
-        (1123, 1, 0.00123),
-        (12123, 2, -0.012315),
-        (12123, 3, 99),
-        (12123, 4, 13),
+        (12123, 1, -123, 1),
+        (12, 1, -1.2, 2),
+        (14, 1, 100, 2),
+        (1381, 1, 9999999.99, 1),
+        (98315, 1, -999999123.123, 1),
+        (1123, 1, 0.00123, 3),
+        (12123, 2, -0.012315, 2),
+        (12123, 3, 99, 1),
+        (12123, 4, 13, 1),
+        (12123, 4, 13, 2),
+        (12123, 4, 13, 3),
     ]
 
     for value in values:
-        crud.add_value(value[0], value[1], value[2])
+        crud.add_value(value[0], value[1], value[3], value[2])
 
     result = None
     with Session() as session:
@@ -36,7 +38,15 @@ def test_add_value(crud_with_session: CrudSession):
         result = session.scalars(query).all()
 
     db_values = list(
-        map(lambda value: (value.time, value.value_type_id, value.value), result)
+        map(
+            lambda value: (
+                value.time,
+                value.value_type_id,
+                value.value,
+                value.device_id,
+            ),
+            result,
+        )
     )
 
     assert all(db_value in values for db_value in db_values)
@@ -45,36 +55,45 @@ def test_add_value(crud_with_session: CrudSession):
 
 def test_add_value_integrity(crud: Crud):
     unique_values = [
-        (1024, 1),
-        (9814, 1),
-        (5123, 1),
-        (1024, 2),
-        (112, 2),
-        (9999, 2),
-        (9999, 3),
-        (9999, 4),
-        (9999, 5),
+        (1024, 1, 1),
+        (9814, 1, 1),
+        (9814, 1, 2),
+        (5123, 1, 2),
+        (1024, 2, 2),
+        (112, 2, 3),
+        (9999, 2, 4),
+        (9999, 3, 4),
+        (9999, 4, 4),
+        (9999, 5, 4),
     ]
 
     for value in unique_values:
-        crud.add_value(value[0], value[1], 100)
+        crud.add_value(value[0], value[1], value[2], 100)
 
     with pytest.raises(IntegrityError):
         for value in unique_values:
-            crud.add_value(value[0], value[1], 100)
+            crud.add_value(value[0], value[1], value[2], 100)
 
 
 def test_add_value_invalid_parameters(crud: Crud):
     values = [
-        (None, None, None),
-        (None, None, 12.3),
-        (None, 15, None),
-        (None, 15, -18.2),
-        (14, None, None),
-        (14, None, 9999),
-        (14, 1, None),
+        (None, None, None, None),
+        (None, None, None, 3),
+        (None, None, 12.3, None),
+        (None, None, 12.3, 2),
+        (None, 15, None, None),
+        (None, 15, None, 14),
+        (None, 15, 12.2, None),
+        (None, 15, 2, 14),
+        (14, None, None, None),
+        (14, None, None, 4),
+        (14, None, 1.1, None),
+        (14, None, 1.1, 17),
+        (14, 12, None, None),
+        (14, 12, None, 1),
+        (14, 12, 101.2, None),
     ]
 
     for value in values:
         with pytest.raises((IntegrityError, TypeError)):
-            crud.add_value(value[0], value[1], value[2])
+            crud.add_value(value[0], value[1], value[3], value[2])
--- tests/test_get_values.py
+++ tests/test_get_values.py
@@ -1,32 +1,43 @@
+import pytest
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from typing import Tuple, Type
 
 from sqlalchemy.orm import Session
 
 from rdp.crud.crud import Crud
-from rdp.crud.model import Value, ValueType
+from rdp.crud.model import Base, Value, ValueType, Device
 
 
 CrudSession = Type[Tuple[Crud, Type[Session]]]
 
 VALUE_TYPES = [("K", "unit1"), ("m", "unit2"), ("s", "unit3")]
 
+DEVICES = ["/dev/test0", "/dev/test1", "/dev/test2", "/dev/test3"]
+
 VALUES = [
-    (1, 1, 12.2),
-    (1, 2, 15.2),
-    (2, 2, -17.1232),
-    (4, 2, 1.2),
-    (5, 2, 6.2),
-    (25, 2, 11),
-    (54, 2, 14),
-    (100, 2, -15.2),
-    (99999, 3, -15.2),
-    (5, 1, -2),
-    (6, 2, 19.3),
+    (1, 1, 12.2, 1),
+    (1, 1, 12.2, 2),
+    (1, 2, 15.2, 1),
+    (2, 2, -17.1232, 1),
+    (4, 2, 1.2, 2),
+    (5, 2, 6.2, 3),
+    (25, 2, 11, 1),
+    (54, 2, 14, 2),
+    (100, 2, -15.2, 3),
+    (99999, 3, -15.2, 4),
+    (5, 1, -2, 1),
+    (6, 2, 19.3, 2),
 ]
 
 
-def test_get_values_all(crud_with_session: CrudSession):
-    crud, Session = crud_with_session
+@pytest.fixture(scope="function")
+def crud_session_values():
+    engine = create_engine("sqlite:///:memory:")
+
+    Session = sessionmaker(bind=engine)
 
     with Session() as session:
         db_value_types = map(
@@ -35,18 +46,34 @@ def test_get_values_all(crud_with_session: CrudSession):
             ),
             VALUE_TYPES,
         )
-        session.add_all(db_value_types)
+        db_devices = map(lambda device: Device(device=device), DEVICES)
+        session.add_all(db_value_types + db_devices)
         session.commit()
         db_values = map(
-            lambda value: Value(time=value[0], value_type_id=value[1], value=value[2]),
+            lambda value: Value(
+                time=value[0], value_type_id=value[1], value=value[2], device=value[3]
+            ),
             VALUES,
         )
         session.add_all(db_values)
         session.commit()
 
+    yield (Crud(engine), Session)
+
+    Base.metadata.drop_all(engine)
+
+
+def test_get_values_all(crud_session_values: CrudSession):
+    crud, Session = crud_session_values
+
     db_values = list(
         map(
-            lambda db_value: (db_value.time, db_value.value_type_id, db_value.value),
+            lambda db_value: (
+                db_value.time,
+                db_value.value_type_id,
+                db_value.value,
+                db_value.device_id,
+            ),
             crud.get_values(),
         )
     )
@@ -55,8 +82,8 @@ def test_get_values_all(crud_with_session: CrudSession):
     assert all(value in db_values for value in VALUES)
 
 
-def test_get_values_by_value_type(crud_with_session: CrudSession):
-    crud, Session = crud_with_session
+def test_get_values_by_value_type(crud_session_values: CrudSession):
+    crud, Session = crud_session_values
 
     with Session() as session:
         db_value_types = map(
@@ -92,8 +119,8 @@ def test_get_values_by_value_type(crud_with_session: CrudSession):
     assert count == len(VALUES)
 
 
-def test_get_values_by_start(crud_with_session: CrudSession):
-    crud, Session = crud_with_session
+def test_get_values_by_start(crud_session_values: CrudSession):
+    crud, Session = crud_session_values
 
     with Session() as session:
         db_value_types = map(
@@ -129,8 +156,8 @@ def test_get_values_by_start(crud_with_session: CrudSession):
         assert all(value in db_values for value in filtered_values)
 
 
-def test_get_values_by_end(crud_with_session: CrudSession):
-    crud, Session = crud_with_session
+def test_get_values_by_end(crud_session_values: CrudSession):
+    crud, Session = crud_session_values
 
     with Session() as session:
         db_value_types = map(
@@ -166,8 +193,8 @@ def test_get_values_by_end(crud_with_session: CrudSession):
         assert all(value in db_values for value in filtered_values)
 
 
-def test_get_values_all_filters(crud_with_session: CrudSession):
-    crud, Session = crud_with_session
+def test_get_values_all_filters(crud_session_values: CrudSession):
+    crud, Session = crud_session_values
 
     with Session() as session:
         db_value_types = map(
