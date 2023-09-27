5d808057e2af9dde6297209c4346674dbc7e0994
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2023-09-27 15:46:40 +0200

    init added api_types.Device

--- rdp/api/api_types.py
+++ rdp/api/api_types.py
@@ -19,3 +19,6 @@ class ApiDescription(BaseModel):
     description : str = "This is the Api"
     value_type_link : str = "/type"
     value_link : str = "/value"
+
+class Device(BaseModel):
+    pass
