9c73ed71c30107fb6407107b101ec38db7a1b00a
Robert Ulmer <rulmer@lr.htlweiz.at>
2024-01-22 20:19:50 +0100

    fix flake8 errors

--- rdp/api/main.py
+++ rdp/api/main.py
@@ -10,24 +10,27 @@ import logging
 logger = logging.getLogger("rdp.api")
 app = FastAPI()
 
+
 @app.get("/")
 def read_root() -> ApiTypes.ApiDescription:
     """This url returns a simple description of the api
 
     Returns:
-        ApiTypes.ApiDescription: the Api description in json format 
-    """    
+        ApiTypes.ApiDescription: the Api description in json format
+    """
     return ApiTypes.ApiDescription()
+
 
 @app.get("/type/")
 def read_types() -> List[ApiTypes.ValueType]:
     """Implements the get of all value types
 
     Returns:
-        List[ApiTypes.ValueType]: list of available valuetypes. 
-    """    
+        List[ApiTypes.ValueType]: list of available valuetypes.
+    """
     global crud
     return crud.get_value_types()
+
 
 @app.get("/type/{id}/")
 def read_type(id: int) -> ApiTypes.ValueType:
@@ -40,14 +43,15 @@ def read_type(id: int) -> ApiTypes.ValueType:
         HTTPException: Thrown if a value type with the given id cannot be accessed
 
     Returns:
-        ApiTypes.ValueType: the desired value type 
+        ApiTypes.ValueType: the desired value type
     """
     global crud
     try:
-         return crud.get_value_type(id)
+        return crud.get_value_type(id)
     except crud.NoResultFound:
-        raise HTTPException(status_code=404, detail="Item not found") 
-    return value_type 
+        raise HTTPException(status_code=404, detail="Item not found")
+    return value_type
+
 
 @app.put("/type/{id}/")
 def put_type(id, value_type: ApiTypes.ValueTypeNoID) -> ApiTypes.ValueType:
@@ -55,23 +59,35 @@ def put_type(id, value_type: ApiTypes.ValueTypeNoID) -> ApiTypes.ValueType:
 
     Args:
         id (int): primary key of the requested value type
-        value_type (ApiTypes.ValueTypeNoID): json object representing the new state of the value type. 
+        value_type (ApiTypes.ValueTypeNoID): json object representing the new state of the value type.
 
     Raises:
-        HTTPException: Thrown if a value type with the given id cannot be accessed 
+        HTTPException: Thrown if a value type with the given id cannot be accessed
 
     Returns:
-        ApiTypes.ValueType: the requested value type after persisted in the database. 
+        ApiTypes.ValueType: the requested value type after persisted in the database.
     """
     global crud
     try:
-        crud.add_or_update_value_type(id, value_type_name=value_type.type_name, value_type_unit=value_type.type_unit)
+        crud.add_or_update_value_type(
+            id,
+            value_type_name=value_type.type_name,
+            value_type_unit=value_type.type_unit,
+        )
         return read_type(id)
     except crud.NoResultFound:
         raise HTTPException(status_code=404, detail="Item not found")
 
+
+@app.get("/value/minmax/")
+def get_minmax_values(type_id: int, start: int = None, end: int = None):
+    return {"min": 0, "max": 1}
+
+
 @app.get("/value/")
-def get_values(type_id:int=None, start:int=None, end:int=None) -> List[ApiTypes.Value]:
+def get_values(
+    type_id: int = None, start: int = None, end: int = None
+) -> List[ApiTypes.Value]:
     """Get values from the database. The default is to return all available values. This result can be filtered.
 
     Args:
@@ -92,10 +108,10 @@ def get_values(type_id:int=None, start:int=None, end:int=None) -> List[ApiTypes.
     except crud.NoResultFound:
         raise HTTPException(status_code=404, deltail="Item not found")
 
+
 @app.on_event("startup")
 async def startup_event() -> None:
-    """start the character device reader
-    """    
+    """start the character device reader"""
     logger.info("STARTUP: Sensor reader!")
     global reader, crud
     engine = create_engine("sqlite:///rdb.test.db")
@@ -104,10 +120,10 @@ async def startup_event() -> None:
     reader.start()
     logger.debug("STARTUP: Sensor reader completed!")
 
+
 @app.on_event("shutdown")
 async def shutdown_event():
-    """stop the character device reader
-    """    
+    """stop the character device reader"""
     global reader
     logger.debug("SHUTDOWN: Sensor reader!")
     reader.stop()
